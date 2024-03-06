392f9bab2d304c7e4707055f12af53d983a67663
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-03-06 12:01:32 +0200

    Add reproduction

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { ExternalProps } from './types';
+
+defineProps<ExternalProps>();
+</script>
+
+<template>
+  <div>
+    <h1>{{ externalProp }}</h1>
+  </div>
+</template>
